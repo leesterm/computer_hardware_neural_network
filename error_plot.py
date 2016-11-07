d4fb6a5534179671539db1bd7031de2e49c8a1b2
Leester Mei <leesterm@gmail.com>
2016-11-06 22:07:50 -0500

    Add average calculations

--- error_plot.py
+++ error_plot.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 data = []
 index = []
 count = 0
+average = 0
 import sys 
 with open(sys.argv[1],'r') as f:
   plt.subplot(111)
@@ -12,8 +13,10 @@ with open(sys.argv[1],'r') as f:
     if line != "_______________\n":
       data.append(float(line))
       index.append(count)
+      average += float(line)
       count+=1
     else:
+      print average/count
       plt.scatter(np.array(index),np.array(data))
       plt.xlabel('Iteration')
       plt.ylabel('Total Output Error')
@@ -21,6 +24,6 @@ with open(sys.argv[1],'r') as f:
       data = []
       index = []
       count = 0
-
+      average = 0
 plt.show()
   
